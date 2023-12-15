971f2cc88efe2110971936f7b9e70c4c67577a4a
Nicholas Adams <njadams.722@gmail.com>
2023-12-15 14:42:08 -0600

    file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+//This code generates the HTML for the REACT project in browser 
+//TODO remove header tag and logo from import
+
+
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      
     </div>
   );
 }
